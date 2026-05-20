37861aa3afd933523b0d73bedd2ff6a097c6347b
Nat Cabrera <source.app.info@gmail.com>
2026-05-20 10:27:16 -0400

    hw2

---
+++ Nat_Cabrera_Modified_Airbnb_Hw_Code/js/main.js
@@ -0,0 +1,63 @@
+function MainModule(listingsID = "#listings") {
+  const me = {};
+
+
+  const listingsElement = document.querySelector(listingsID);
+
+  function getListingCode(listing, index) {
+    const colors = ["#cfe2f3","#d9d2e9", "#d9ead3", "#ffbe98"];
+    const color = colors[index % colors.length];
+    return `<div class="col-4">
+  <div class="listing card" style="background-color: ${color}">
+    <img
+      src="${listing.picture_url}"
+      class="card-img-top"
+      alt="AirBNB Listing"
+    />
+    <div class="card-body">
+      <h2 class="card-title">${listing.name}</h2>
+      <div>${listing.price}</div>
+      <img src="${listing.host_thumbnail_url}" class="thumbnail" /> 
+      <p class="description">${listing.description}</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
+  <!-- /card -->
+  </div>
+
+  `;
+  }
+
+  function redraw(listings) {
+    listingsElement.innerHTML = "";
+    // for (let i = 0; i < listings.length; i++) {
+    //   listingsElement.innerHTML += getListingCode(listings[i]);
+    // }
+
+    // for (let listing of listings) {
+    //   console.log("listing", listing );
+    //   listingsElement.innerHTML += getListingCode(listing);
+    // }
+
+    listingsElement.innerHTML = listings.map(getListingCode).join("\n");
+  }
+
+  async function loadData() {
+    const res = await fetch("./airbnb_sf_listings_500.json");
+    const listings = await res.json();
+    const withImages = listings.filter(listing => listing.picture_url);
+
+
+    me.redraw(withImages.slice(0, 50));
+  }
+
+  me.redraw = redraw;
+  me.loadData = loadData;
+
+  return me;
+}
+
+const main = MainModule();
+
+
+main.loadData();
